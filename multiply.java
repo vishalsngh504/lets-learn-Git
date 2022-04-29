cd3f9c37376f2af70e9b9aa48fac817ac4f3d8c5
vishalsngh504 <vishalsngh504@gmail.com>
2022-04-29 16:43:58 +0530

    added mutiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+
+    public static void main(string[] args) {
+        init a = 4;
+        init b = 2;
+
+        system.out.println("the product is " + (a*b))
+    }
+
+}
